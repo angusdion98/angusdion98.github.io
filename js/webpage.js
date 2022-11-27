cf22be3014acfba2318b28d31b1ccfe2ea677868
angusdion98 <118675653+angusdion98@users.noreply.github.com>
2022-11-27 17:37:35 +0000

    Add files via upload

---
+++ js/webpage.js
@@ -0,0 +1,10 @@
+const observer = newIntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const Elements = document.querySelectorAll('.hidden');
